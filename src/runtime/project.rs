0c11fc24fba20f7d6c45b3327dcc319864f742a2
Alphaly2K <52882149+Alphaly2K@users.noreply.github.com>
2026-08-01 03:02:55 +0800

    Revert "fix: treat zero-duration transitions as immediate commits"
    This reverts commit 0772db2e0503a2bd088025d6d1139cf633a98588.

--- src/runtime/project.rs
+++ src/runtime/project.rs
@@ -227,14 +227,7 @@ fn event_requires_host_pause(e: &Event) -> bool {
         e,
         Event::Wait { .. } | Event::YesNo { .. } | Event::ShowDialog { .. }
     ) || matches!(e, Event::VideoPlay { id, .. } if id.is_none())
-        || matches!(
-            e,
-            Event::Trans {
-                trans_type,
-                time,
-                ..
-            } if crate::render_pipeline::transition::is_animated_request(*trans_type, *time)
-        )
+        || matches!(e, Event::Trans { trans_type, .. } if *trans_type != 0)
 }
 
 #[cfg(test)]
@@ -245,27 +238,6 @@ mod tests {
         Arc,
         atomic::{AtomicBool, Ordering},
     };
-
-    #[test]
-    fn zero_duration_transition_does_not_pause_the_interpreter() {
-        let immediate = Event::Trans {
-            trans_type: 1,
-            time: Some(0),
-            rule: None,
-            vague: None,
-            input: 1,
-        };
-        let animated = Event::Trans {
-            trans_type: 1,
-            time: Some(1),
-            rule: None,
-            vague: None,
-            input: 1,
-        };
-
-        assert!(!event_requires_host_pause(&immediate));
-        assert!(event_requires_host_pause(&animated));
-    }
 
     #[test]
     fn go_title_reset_triggers_event() {
